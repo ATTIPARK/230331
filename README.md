c957c338bca3200632bf5c2bb990ef727bd18dd0
ATTIPARK <qkrqjawls0922@gmail.com>
2023-03-31 13:15:44 +0900

    make calculator

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # 230331
+
+1
+first commit
+-useState() 사용법
+
+2
+make calculator
+-useState() 사용해서 간단한 계산기 만들기
--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,90 @@
 import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [num1, setNum1] = useState();
+  const [num2, setNum2] = useState();
+  const [result, setResult] = useState(0);
 
-  const onClickAdd = () => {
-    setCount(count + 1);
+  const onChangeNum1 = (event) => {
+    setNum1(event.target.value);
   };
-  const onClickSubtract = () => {
-    // setCount((prev) => {
-    //   return prev - 1;
-    // });
+  const onChangeNum2 = (event) => {
+    setNum2(event.target.value);
+  };
 
-    if (count <= 0) {
-      alert("count의 값이 0보다 작을수 없습니다!");
+  const check = (n1, n2) => {
+    if (n1 === "" || n2 === "") {
+      alert("값을 입력해주세요.");
       return;
     }
-
-    setCount(count - 1);
+    if (isNaN(n1) || isNaN(n2)) {
+      alert("숫자를 입력해주세요.");
+      return;
+    }
+  };
+  const onClickAdd = () => {
+    check(num1, num2);
+    setResult(parseInt(num1) + parseInt(num2));
+  };
+  const onClickSubtract = () => {
+    check(num1, num2);
+    setResult(parseInt(num1) - parseInt(num2));
+  };
+  const onClickMultiply = () => {
+    check(num1, num2);
+    setResult(parseInt(num1) * parseInt(num2));
+  };
+  const onClickDivide = () => {
+    check(num1, num2);
+    setResult(parseInt(num1) / parseInt(num2));
   };
 
   return (
     <div className="bg-red-100 min-h-screen flex flex-col justify-center items-center">
-      <div>{count}</div>
-      <button
-        className="bg-green-300 w-12 h-12 rounded-full"
-        onClick={onClickAdd}
-      >
-        +
-      </button>
-      <button
-        className="bg-red-300 w-12 h-12 rounded-full"
-        onClick={onClickSubtract}
-      >
-        -
-      </button>
+      <h1 className="text-2xl font-bold mb-4">Awesome한 계산기(로 바뀔예정)</h1>
+      <div className="text-3xl font-black mb-4">{result}</div>
+      <div className="mb-4">
+        <input
+          className="border-2 border-gray-800 focus:outline-none focus:border-purple-400 rounded-lg px-4 py-2 text-xl ml-4"
+          type="text"
+          value={num1}
+          onChange={onChangeNum1}
+        />
+        <input
+          className="border-2 border-gray-800 focus:outline-none focus:border-purple-400 rounded-lg px-4 py-2 text-xl ml-4"
+          type="text"
+          value={num2}
+          onChange={onChangeNum2}
+        />
+      </div>
+      <div>
+        <button
+          onClick={onClickAdd}
+          className="border-2 px-2 py-1 rounded-lg border-red-500 text-red-500"
+        >
+          Add
+        </button>
+        <button
+          onClick={onClickSubtract}
+          className="border-2 px-2 py-1 rounded-lg border-blue-500 text-blue-500 ml-4"
+        >
+          Subtract
+        </button>
+
+        <button
+          onClick={onClickMultiply}
+          className="border-2 px-2 py-1 rounded-lg border-yellow-500 text-yellow-500 ml-4"
+        >
+          Multiply
+        </button>
+
+        <button
+          onClick={onClickDivide}
+          className="border-2 px-2 py-1 rounded-lg border-green-500 text-green-500 ml-4"
+        >
+          Divide
+        </button>
+      </div>
     </div>
   );
 }
